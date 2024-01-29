3cf3c0d8de2f7e1cee5819c04d8aa9bc7b1a0503
Andre <andre.luismp@gmail.com>
2024-01-29 11:04:50 +0000

    feat: auth set up

--- client/App.tsx
+++ client/App.tsx
@@ -1,12 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import login from './components/login/login';
+
+const Stack = createNativeStackNavigator();
+const getIsSignedIn = () => true;
 
 export default function App() {
+  const isSignedIn = getIsSignedIn();
+
   return (
-    <View style={styles.container}>
-      <Text>Suuuuiii</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        {isSignedIn ? (
+          <>
+            <View style={styles.container}>
+              <Text>Suuuuiii</Text>
+              <StatusBar style="auto" />
+            </View>
+          </>
+        ) : (
+          <>
+            <Stack.Screen name="login" component={login} />
+          </>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
