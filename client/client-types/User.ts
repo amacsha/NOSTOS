1742cdfc63742316f9cc5aa7416bb449e9699d82
Andrew MacShane <andrew.macshane@icloud.com>
2024-01-29 16:44:24 +0000

    feat: configured redux, added type files, added component files

---
+++ client/client-types/User.ts
@@ -0,0 +1,11 @@
+export interface UserState {
+  id: number | null;
+  email: string | null;
+  username: string | null;
+  filter_preference: string | null;
+  isAuthenticated: boolean;
+  entryIds?: number[];
+  ratingIds?: number[];
+  commentIds?: number[];
+  lastVisitedIds?: number[];
+}
