c15d0c14f5211975c468fee42eda0ab874e327ae
diegoss-github <diegoss@bu.edu>
2024-01-29 19:45:42 +0000

    added jwt authentication to backend

--- server/src/middlewares/auth.ts
+++ server/src/middlewares/auth.ts
@@ -1,12 +1,23 @@
 import Koa from 'koa';
+const jwt = require("jsonwebtoken");
 const auth_client = require('../models/db');
+const SECRET_KEY = process.env.SECRET_KEY
 
 const authMiddleware = async (ctx: Koa.Context, next: any) => {
+  const authHeaders: any = ctx.headers["authorization"];
+  if (!authHeaders) {
+    ctx.status = 403
+  }
+  const token = authHeaders.split(" ")[1];
   try {
-    const { uid } = ctx.session;
-    const user = await auth_client.user.findUnique({ id: uid });
-    if (!user) throw new Error();
-    ctx.user = user;
+    // verify & decode token payload,
+    const { _id } = jwt.verify(token, SECRET_KEY);
+    // attempt to find user object and set to req
+    const user = await auth_client.findOne({ _id });
+    if (!user) {
+      ctx.status = 401;
+    } 
+    ctx.body = user;
     next();
   } catch (error) {
     console.error(error);
--- server/src/router.ts
+++ server/src/router.ts
@@ -1,16 +1,15 @@
 const Router = require('@koa/router');
 const router = new Router();
 import { getLastVisits, setLastVisit } from './controllers/lastVisited.controller';
-import {createOneUser, deleteUser, getOneUser, getUserFilterPreference, setUserFilterPreference} from './controllers/user.controller';
+import {createOneUser, deleteUser, getOneUser, getUserFilterPreference, loginUser, logoutUser, setUserFilterPreference} from './controllers/user.controller';
 import {authMiddleware} from './middlewares/auth'
 
 router.post('/user/createOneUser', createOneUser);
 router.get('/user/getOneUser/:id', getOneUser);
 router.delete('/user/deleteUser/:id', deleteUser);
 
-router.post('/register', create);
-router.post('/login', login);
-// router.post('/logout', authMiddleware, logout);
+router.post('/login', loginUser);
+router.post('/logout', authMiddleware, logoutUser);
 
 router.put('/user/setUserFilterPreference/:id', setUserFilterPreference);
 router.get('/user/getUserFilterPreference/:id', getUserFilterPreference);
