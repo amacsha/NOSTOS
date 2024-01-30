03aa6e8b1e04acb3ab11bb9fd932fb3fe9349373
Dominic Stewart-Smith <dominicstewartsmith@gmail.com>
2024-01-30 08:36:48 +0000

    update gitignore

--- server/dist/router.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const Router = require('@koa/router');
-const router = new Router();
-const createUser = require('./controllers/user.controller');
-router.post('/user/createOneUser', createUser);
-module.exports = router;
