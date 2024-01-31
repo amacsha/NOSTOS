9d795b2512807c59c0f1e970d6080c3533f97061
Andre <andre.luismp@gmail.com>
2024-01-31 10:41:47 +0000

    feat: formik and yup

--- client/components/login/login.tsx
+++ client/components/login/login.tsx
@@ -1,62 +1,72 @@
 import React, { useState } from "react";
 import { Alert, Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { Formik, useFormik } from "formik";
+import * as Yup from 'yup';
 import loginService from "./loginService";
 import { LoginValues } from "../../client-types/LoginValues";
 import { useAppDispatch } from '../../hooks';
 import { setAuth, initialState } from '../../slices/authSlice';
 import { save } from "../../utils/secureStorage";
-import LogoutService from "./logoutService";
 
 
 const Login: React.FC = () => {
-  const [loginForm, setLoginForm] = useState<LoginValues>({ email: "", password: "" })
-  const [error, setError] = useState<LoginValues>({ email: "", password: "" })
+  const validationSchema = Yup.object().shape({
+    email: Yup.string()
+      .email('Invalid email')
+      .required('Email is required'),
+    password: Yup.string()
+      .required('Password is required'),
+  })
 
   const dispatch = useAppDispatch();
 
-  const handleChange = (name: keyof typeof loginForm, value: string) => {
-    setLoginForm({ ...loginForm, [name]: value })
-  }
-
-  const handleSubmit = async () => {
-    let newError: LoginValues = { ...error }
-    !loginForm.email ? newError.email = "email is required" : newError.email = "";
-    !loginForm.password ? newError.password = "Password is required" : newError.password = "";
-    setError(newError)
-
-    if (Object.values(newError).every(err => err === "")) {
-      const res: any = await loginService(loginForm)
-      if (res.error) {
-        Alert.alert(`${res.error}`);
-        dispatch(setAuth(initialState));
-      } else {
-        Alert.alert('login 👍')
-        dispatch(setAuth({ isAuthenticated: true, token: res.data }))
-        save('accessToken', res.data);
-      }
+  const handleSubmit = async (values: LoginValues) => {
+    const res: any = await loginService(values)
+    if (res.error) {
+      Alert.alert(`${res.error}`);
+      dispatch(setAuth(initialState));
+    } else {
+      Alert.alert('login 👍')
+      dispatch(setAuth({ isAuthenticated: true, token: res.data }))
+      save('accessToken', res.data);
     }
   }
 
   return (
-    <>
-      <View style={styles.container}>
-        <Text style={styles.head}>Login</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="email"
-          onChange={(event) => handleChange("email", event.nativeEvent.text)}
-        />
-        {error.email !== "" && <Text style={styles.error}>{error.email}</Text>}
-        <TextInput
-          style={styles.input}
-          placeholder="Password"
-          onChange={(event) => handleChange("password", event.nativeEvent.text)}
-        />
-        {error.password !== "" && <Text style={styles.error}>{error.password}</Text>}
-        <Button title="Login" onPress={handleSubmit} />
-      </View>
-      <LogoutService />
-    </>
+    <View style={styles.container}>
+      <Text style={styles.head}>Login</Text>
+      <Formik
+        initialValues={{ email: '', password: '' }}
+        validationSchema={validationSchema}
+        onSubmit={values => handleSubmit(values)}
+      >
+        {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
+          <>
+            <TextInput
+              style={styles.input}
+              placeholder="Email"
+              onChangeText={handleChange("email")}
+              onBlur={handleBlur('email')}
+              value={values.email}
+            />
+            {touched.email && errors.email && (
+              <Text style={styles.error}>{errors.email}</Text>
+            )}
+            <TextInput
+              style={styles.input}
+              placeholder="Password"
+              onChangeText={handleChange("password")}
+              onBlur={handleBlur("password")}
+              value={values.password}
+            />
+            {touched.password && errors.password && (
+              <Text style={styles.error}>{errors.password}</Text>
+            )}
+            <Button title="Login" onPress={handleSubmit} />
+          </>
+        )}
+      </Formik>
+    </View >
   )
 }
 
