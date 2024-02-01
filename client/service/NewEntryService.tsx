6562fd3e33bbb7dc989de0e8050c1dc1803daa6c
Andre <andre.luismp@gmail.com>
2024-02-01 14:02:11 +0000

    fix: user login

--- client/service/NewEntryService.tsx
+++ client/service/NewEntryService.tsx
@@ -5,7 +5,7 @@ const IP: string | undefined = process.env.EXPO_PUBLIC_IP_ADDRESS;
 
 const NewEntryService = async (newEntry: Entry) => {
     console.log(IP)
-    const res = await axios.post(`${IP}/entry/addOne`, newEntry)
+    const res = await axios.post(`http://${IP}:3000/entry/addOne`, newEntry)
     console.log(res)
     return res
 }
