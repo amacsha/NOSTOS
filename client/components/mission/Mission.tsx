daa218e86d74ead217a5d818403dea54fbae6cb6
diegoss-github <diegoss@bu.edu>
2024-02-03 13:25:26 +0000

    feat: setup for geofencing authentication

--- client/components/mission/Mission.tsx
+++ client/components/mission/Mission.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import {Button, Text, Pressable, StyleSheet, View, Linking,} from "react-native";
-import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import MapView, { Circle, Marker, PROVIDER_GOOGLE } from "react-native-maps";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../store";
 import { selectPlace, setPlaces } from "../../slices/placesSlice";
@@ -9,15 +9,29 @@ import { GooglePlaceResponse, Place } from "../../client-types/Place";
 import AddPlacesService from "../../service/AddPlacesService";
 import { fetchNewMissions } from "../../service/NewMissionService";
 import * as Haptics from "expo-haptics";
+import mapStyle from "./MapStyle";
+import { GeofencingEventType } from 'expo-location';
+import * as TaskManager from 'expo-task-manager';
+import * as Location from 'expo-location';
 
 const GOOGLE_KEY = process.env.EXPO_PUBLIC_GOOGLE_API_KEY;
+
+interface GeofencingData {
+  eventType: GeofencingEventType;
+  region: {
+    identifier: string;
+    latitude: number;
+    longitude: number;
+    radius: number;
+    // Add any other properties you may need
+  };
+}
 
 const Mission: React.FC = ({ navigation }: any) => {
   const dispatch = useDispatch();
   const location = useSelector((state: RootState) => state.location);
   const places = useSelector((state: RootState) => state.places.places);
   const city = useSelector((state: RootState) => state.location.value?.cityName);
-  const placeId = useSelector((state: RootState) => state.places.selectedPlaceId);
 
   const [selectedMarker, setSelectedMarker] = useState(false);
   const [selectedCoord, setSelectedCoord] = useState<number[]>([]);
@@ -25,44 +39,50 @@ const Mission: React.FC = ({ navigation }: any) => {
   const lat = location.value?.lat;
   const lng = location.value?.lng;
 
-  // useEffect(() => {
-  //   axios
-  //     .get("https://maps.googleapis.com/maps/api/place/nearbysearch/json", {
-  //       params: {
-  //         location: `${lat},${lng}`,
-  //         radius: 10000,
-  //         type: "hindu_temple",
-  //         key: GOOGLE_KEY,
-  //       },
-  //     })
-  //     .then((response) => {
-  //       const places: Place[] = response.data.results.map(
-  //         (place: GooglePlaceResponse) => ({
-  //           id: place.place_id,
-  //           lat: place.geometry.location.lat,
-  //           lng: place.geometry.location.lng,
-  //           name: place.name,
-  //           city: "London", // TODO receive actual city upon location at login (get from state)
-  //         })
-  //       );
-  //       AddPlacesService(places);
-  //       dispatch(setPlaces(places));
-  //     })
-  //     .catch((error) => console.log(error));
-  // }, []);
-
+  
   useEffect(() => {
-    city && fetchNewMissions(city, dispatch);
+    if (city){
+      fetchNewMissions(city, dispatch);
+    }
   }, [city]);
-
+  
   function handleMarkerPress(place: Place, latitude: number, longitude: number) {
     axios
-      .get(`https://maps.googleapis.com/maps/api/directions/json?destination=${latitude},${longitude}&origin=${lat},${lng}&key=${GOOGLE_KEY}`)
-      .then((response) => response)
-      .catch((error) => console.log(error));
+    .get(`https://maps.googleapis.com/maps/api/directions/json?destination=${latitude},${longitude}&origin=${lat},${lng}&key=${GOOGLE_KEY}`)
+    .then((response) => response)
+    .catch((error) => console.log(error));
     setSelectedCoord([latitude, longitude]);
     dispatch(selectPlace(place.id));
     setSelectedMarker(true);
+  }
+    
+  TaskManager.defineTask('startGeofence', ({ data, error}: {data: GeofencingData; error?: any}) => {
+    if (error) {
+      console.log(error)
+      return;
+    }
+
+    const { eventType, region} = data;
+
+    if (eventType === GeofencingEventType.Enter) {
+      console.log("You've entered region:", region);
+    } else if (eventType === GeofencingEventType.Exit) {
+      console.log("You've left region:", region);
+    }
+  });
+
+  const placesToGeofence = () => {
+    places.map(place => ({
+      latitude: place.lat,
+      longitude: place.lng,
+      radius: 100
+    }))
+  }
+
+  console.log(placesToGeofence)
+
+  const startGeofence = async () => {
+    await Location.startGeofencingAsync('startGeofence', placesToGeofence);
   }
   
   return (
@@ -70,6 +90,7 @@ const Mission: React.FC = ({ navigation }: any) => {
         style={styles.map}
         provider={PROVIDER_GOOGLE}
         showsUserLocation={true}
+        customMapStyle={mapStyle}
         initialRegion={{
           latitude: location.value?.lat ?? 0,
           longitude: location.value?.lng ?? 0,
@@ -78,15 +99,22 @@ const Mission: React.FC = ({ navigation }: any) => {
         }}
       >
         {places.map((place, index) => (
+          <React.Fragment key={index}>
+          <Circle
+            center={{ latitude: place.lat, longitude: place.lng }}
+            radius={100}
+            fillColor="rgba(255, 0, 0, 0.3)"
+          />
           <Marker
-            key={index}
             coordinate={{ latitude: place.lat, longitude: place.lng }}
+            icon={require('../../assets/mission-marker.png')}
             title={place.name}
             onPress={() => {
-              handleMarkerPress(place, place.lat, place.lng)
-              Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light)
+              handleMarkerPress(place, place.lat, place.lng);
+              Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
             }}
           />
+        </React.Fragment>
         ))}
         {selectedMarker && (
           <View>
