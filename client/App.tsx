6a8ac361db2942e579048794ae9bf537f8c33d9c
Andrew MacShane <andrew.macshane@icloud.com>
2024-02-03 11:59:33 +0000

    feat: fixed font warnings

--- client/App.tsx
+++ client/App.tsx
@@ -1,17 +1,31 @@
-import React from "react";
-import { StyleSheet, useCallback } from "react-native";
+import React, { useCallback, useEffect } from "react";
+import { StyleSheet, View, Text } from "react-native";
 import UserStart from "./components/login/UserStart";
-import { useFonts } from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen';
+import { useFonts } from 'expo-font';
 import store from "./store";
 import { Provider } from "react-redux";
 
 export default function App() {
+  SplashScreen.preventAutoHideAsync(); 
 
   const [fontLoaded, fontError] = useFonts({
     'Gruppe_A': require('./assets/fonts/Gruppe_A.ttf'),
-  })
+  });
 
-  
+  const onLayoutRootView = useCallback(async () => {
+    if (fontLoaded || fontError) {
+      await SplashScreen.hideAsync(); 
+    }
+  }, [fontLoaded, fontError]);
+
+  useEffect(() => {
+    onLayoutRootView();
+  }, [onLayoutRootView]);
+
+  if (!fontLoaded) {
+    return null; 
+  }
 
   return (
     <Provider store={store}>
@@ -26,9 +40,5 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-  },
-  map: {
-    width: '100%',
-    height: '100%'
   },
 });
