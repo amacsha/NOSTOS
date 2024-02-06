02d8ae5b060c81c4258b0cd4032af2adfec039ef
Andre <andre.luismp@gmail.com>
2024-02-06 11:36:18 +0000

    fix: new entry style

--- client/components/navbar/Navbar.tsx
+++ client/components/navbar/Navbar.tsx
@@ -17,7 +17,7 @@ export default function Navbar() {
             screenOptions={{ headerShown: false,
             tabBarActiveBackgroundColor: colors.basePurple}}
         >
-            <Tab.Screen name="Logs" component={Main} />
+            <Tab.Screen name="NewEntryFrom" component={NewEntryForm} />
             <Tab.Screen name="GlobeView" component={GlobeView} />
             <Tab.Screen name="Mission" component={Mission} />
             <Tab.Screen name="UserProfile" component={UserProfile} />
