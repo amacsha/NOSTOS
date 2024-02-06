10e69eb8bffdcc1386d49f53d2439d8f5ffb7c86
Andre <andre.luismp@gmail.com>
2024-02-06 16:17:05 +0000

    fix: navbar

--- client/components/navbar/Navbar.tsx
+++ client/components/navbar/Navbar.tsx
@@ -7,6 +7,10 @@ import Home from '../Home/Home';
 import GlobeView from '../dashboard/GlobeView';
 import NewEntryForm from '../new-entry/NewEntryForm';
 import { colors } from '../styles/colors';
+import { FontAwesome6 } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
 
 const Tab = createBottomTabNavigator();
 
@@ -22,19 +26,52 @@ export default function Navbar() {
                 tabBarShowLabel: false,
                 tabBarStyle: {
                     backgroundColor: colors.darkGrey,
-                    height: 60,
-                    borderTopWidth: 0,// Remove border on the top
-                    shadowOpacity: 0, // Remove shadow on iOS
-                    elevation: 0, // Remove shadow on Android
+                    height: 90,
+                    // borderTopWidth: 5,
+                    // borderBottomWidth: 5,
+                    // marginVertical: 5,
 
+                    // paddingBottom: 15,
+                    borderTopColor: colors.darkGrey,
+                    shadowOpacity: 3,
+                    shadowColor: colors.lighterPurple,
+                    elevation: 15,
                 },
 
             }}
         >
-            <Tab.Screen name="Logs" component={Main} />
-            <Tab.Screen name="GlobeView" component={GlobeView} />
-            <Tab.Screen name="Mission" component={Mission} />
-            <Tab.Screen name="UserProfile" component={UserProfile} />
+            <Tab.Screen name="Logs" component={Main}
+                options={{
+                    tabBarLabel: 'Home',
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons name="card-text-outline" size={25} color="white" />
+                    ),
+                }}
+            />
+            <Tab.Screen name="GlobeView" component={GlobeView}
+                options={{
+                    tabBarLabel: 'Home',
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons name="rocket" size={24} color="white" />
+                    ),
+                }}
+            />
+            <Tab.Screen name="Mission" component={Mission}
+                options={{
+                    tabBarLabel: 'Home',
+                    tabBarIcon: ({ color, size }) => (
+                        <AntDesign name="earth" size={25} color="white" />
+                    ),
+                }}
+            />
+            <Tab.Screen name="UserProfile" component={UserProfile}
+                options={{
+                    tabBarLabel: 'Home',
+                    tabBarIcon: ({ color, size }) => (
+                        <FontAwesome6 name="user-astronaut" size={25} color={colors.gunMetalGrey} />
+                    ),
+                }}
+            />
         </Tab.Navigator>
     );
 }
